35b3c99b517a32780068803562ea33ba0654e1b6
Tarun Pottigari <pottigarit@dnb.com>
2024-06-22 19:23:43 +0530

    todays activity

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -16,12 +17,7 @@ const StyledDashboardLayout = styled.div`
 
 export default function DashboardLayout() {
   const { bookings, isLoading: isLoading1 } = useRecentBookings();
-  const {
-    stays,
-    isLoading: isLoading2,
-    confirmedStays,
-    numsDays,
-  } = useRecentStays();
+  const { isLoading: isLoading2, confirmedStays, numsDays } = useRecentStays();
   const { cabins, isLoading: isLoading3 } = useCabins();
   if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
 
@@ -33,7 +29,7 @@ export default function DashboardLayout() {
         numDays={numsDays}
         cabinCount={cabins.length}
       />
-      <div>Todays Activity</div>
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numsDays} />
     </StyledDashboardLayout>
